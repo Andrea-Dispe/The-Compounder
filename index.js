f0075c936b85fd75edd9ae8c933c37f54798a6cb
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2021-03-22 10:42:55 +0800

    something

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-const interest =  1 +  (0.05 / 12);
+const arrayTable = [];
 
-let cap = 160_000;
-const time = 240;
-const increment = 1_000;
+// const interest =  1 +  (0.036 / 12);
 
-function calcInt(interest, cap, time, increment) {
+// let cap = 200_000;
+let cap = 250_000;
+const time = 25*12;
+const increment = 1000;
+
+const result = [{}]
+
+
+// function calcInt(interest, cap, time, increment) {
+//   for (let i = 0; i < time; i++) {
+
+//     cap = (cap + increment) * interest;
+
+//     arrayTable.push(cap);
+//   }
+//   console.table(arrayTable);
+//   return cap
+// }
+
+// calcInt(interest, cap, time, increment)
+
+const dividendQuarter = 0.036/4;
+
+let interest;
+
+
+// DRIP no so drip
+function drip(interest, cap, time, increment) {
+  let counter = 0
   for (let i = 0; i < time; i++) {
 
-    cap = (cap + increment) * interest
-    console.log('cap: ', cap);
+    if( i % 3 === 0) {
+    counter++
+      interest = cap * dividendQuarter;
+      cap += interest
+      console.log(cap, interest)
+    }
   }
-  return cap
+  return counter
 }
 
-calcInt(interest, cap, time, increment)
+drip(interest, cap, time, increment)
